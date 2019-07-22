d48d5fdfed077ce1bd02fd2e3a4d08a5a1d83a65
Michał Rypina <michalpypek@op.pl>
2019-07-22 12:55:11 +0200

    Calculating normals manually

--- Terrain Generation/Assets/Code/NoiseGeneration/MeshData.cs
+++ Terrain Generation/Assets/Code/NoiseGeneration/MeshData.cs
@@ -25,12 +25,51 @@ public class MeshData
 		lastTriangleIndex += 3;
 	}
 
+	private Vector3[] CalculateNormals()
+	{
+		Vector3[] vertexNormals = new Vector3[vertices.Length];
+		int triangleCount = triangles.Length / 3;
+
+		for (int i = 0; i < triangleCount; i++)
+		{
+			int normalTriangleIndex = i * 3;
+			int vertexIndexA = triangles[normalTriangleIndex];
+			int vertexIndexB = triangles[normalTriangleIndex + 1];
+			int vertexIndexC = triangles[normalTriangleIndex + 2];
+
+			var triangleNormal = SurfaceNormalFromIndices(vertexIndexA, vertexIndexB, vertexIndexC);
+			vertexNormals[vertexIndexA] += triangleNormal;
+			vertexNormals[vertexIndexB] += triangleNormal;
+			vertexNormals[vertexIndexC] += triangleNormal;
+		}
+
+		for (int i = 0; i < vertexNormals.Length; i++)
+		{
+			vertexNormals[i].Normalize();
+		}
+
+		return vertexNormals;
+	}
+
+	private Vector3 SurfaceNormalFromIndices(int indA, int indB, int indC)
+	{
+		var vertA = vertices[indA];
+		var vertB = vertices[indB];
+		var vertC = vertices[indC];
+
+		var edgeAB = vertB - vertA;
+		var edgeAC = vertC - vertA;
+
+		return Vector3.Cross(edgeAB, edgeAC).normalized;
+	}
+
 	public Mesh GetMesh()
 	{
 		Mesh mesh = new Mesh();
 		mesh.vertices = vertices;
 		mesh.uv = uvs;
 		mesh.triangles = triangles;
+		mesh.normals = CalculateNormals();
 		mesh.RecalculateNormals();
 		return mesh;
 	}
